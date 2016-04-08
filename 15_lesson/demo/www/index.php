e4987755273b63727fb14f147abbf0cbf66b85a0
Alexander Schilling <dignityinside@gmail.com>
2016-04-08 17:37:24 +0200

    Add source of 15-th lesson

---
+++ 15_lesson/demo/www/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Логические операторы в PHP </title>
+</head>
+<body>
+    <?php
+
+        // !$a; # Не (NOT), отрицание, true если $a не true
+        // $a and $b; # И (AND, &&), логическое И, true если и $a, и $b true
+        // $a or $b; # ИЛИ (OR, ||) логическое ИЛИ, true если или $a, или $b true
+
+    ?>
+</body>
+</html>
