c622cf3d009e6304316dada923a9a3090df77d9b
Alexander Schilling <dignityinside@gmail.com>
2016-04-08 17:10:38 +0200

    Add source of eleventh lesson

---
+++ 11_lesson/demo/www/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Арифметические операторы в PHP</title>
+</head>
+<body>
+    <?php
+
+        $a = 10;
+        $b = 5;
+
+        // сложение
+        $sum = $a + $b;
+
+        // вычитание
+        $sum = $a - $b;
+
+        // умножение
+        $sum = $a * $b;
+
+        // деление
+        $sum = $a / $b;
+
+        // остаток от деления (актуально для целых чисел)
+        $sum = $a % $b;
+
+        echo $sum;
+
+    ?>
+</body>
+</html>
