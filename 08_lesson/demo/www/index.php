bf66585834baff250952557c51026fb8ce595efe
Alexander Schilling <dignityinside@gmail.com>
2016-04-08 16:57:38 +0200

    Add source of eighth lesson

---
+++ 08_lesson/demo/www/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Привет мир!</title>
+</head>
+<body>
+    <?php
+
+        $name = "Привет мир!";
+
+        echo $name;
+
+    ?>
+</body>
+</html>
