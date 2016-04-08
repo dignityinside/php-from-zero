cd9e95860015240e583ac2bf4b8eb45b7ba14dbf
Alexander Schilling <dignityinside@gmail.com>
2016-04-08 16:54:06 +0200

    Add source of seventh lesson

---
+++ 07_lesson/demo/www/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Привет мир!</title>
+</head>
+<body>
+    <?php
+
+        # комментарий
+
+        // комментарий
+
+        /*
+
+        ...
+
+         Длинный комментарий
+
+        ...
+
+         */
+
+        echo "Я ищучаю PHP!";
+
+    ?>
+</body>
+</html>
