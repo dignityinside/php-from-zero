da62665ed16518e1f3cfd33d07c27d9f3b912389
Alexander Schilling <dignityinside@gmail.com>
2016-04-08 18:48:02 +0200

    Add source of 21-th lesson

---
+++ 21_lesson/demo/www/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>GET и POST + Работа с формами</title>
+</head>
+<body>
+<?php
+
+    // GET
+
+    // print_r($_GET);
+    // $name = $_GET["name"];
+    // echo "Привет, " . $name;
+
+?>
+<!-- <form action="send.php" method="get"> -->
+<form action="send.php" method="post">
+    <p>Ваше имя: <input type="text" name="name"/></p>
+    <p><input type="submit" value="Отправить"/></p>
+</form>
+</body>
+</html>
