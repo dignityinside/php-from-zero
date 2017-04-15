877879e745e4ce9af7bc11d0a93f90b74dcf516a
Alexander Schilling <dignityinside@gmail.com>
2017-04-15 23:54:17 +0200

    Add source code for is type lesson

---
+++ 29_lesson/demo/www/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$arr = ['alex', 123];
+
+echo is_array($arr);
+
+echo is_integer(123);
+
+echo is_double(1.5);
+
+echo is_string('hello');
+
+echo is_numeric(1.5);
+
+echo is_bool(true);
+
+echo is_null(null);
+
+echo is_scalar('str');
+
+var_dump($arr);
