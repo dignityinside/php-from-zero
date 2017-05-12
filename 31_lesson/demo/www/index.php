f1bc6cdb0d5f252f18ab00d3ec4cb9126565b3a0
Alexander Schilling <dignityinside@gmail.com>
2017-05-12 18:11:11 +0200

    Add source of 31 lesson

---
+++ 31_lesson/demo/www/index.php
@@ -0,0 +1,17 @@
+<?php
+
+$str = 'hello world';
+
+echo strlen($str);
+
+echo substr($str, 0, 5);
+
+echo str_replace('hello', 'privet', $str);
+
+echo strtolower($str);
+
+echo strtoupper($str);
+
+echo md5($str);
+
+echo trim($str);
