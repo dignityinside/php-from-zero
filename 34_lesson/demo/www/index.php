2643a497774f286c0aa6827172a02ea5d3a0d433
Alexander Schilling <dignityinside@gmail.com>
2017-05-13 11:23:12 +0200

    Add source of 34 lesson

---
+++ 34_lesson/demo/www/index.php
@@ -0,0 +1,15 @@
+<?php
+
+// file_put_contents('log.txt', 'привет мир');
+
+// echo file_get_contents('log.txt');
+
+//if (file_exists('log2.txt')) {
+//    echo file_get_contents('log.txt');
+//}
+
+//echo filesize('log.txt');
+
+// rename('log.txt', 'new_log.txt');
+
+// unlink('new_log.txt');
